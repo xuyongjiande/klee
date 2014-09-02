009f6fe8ab7dd99036681712e85549b5e17b5389
Yongjian Xu <xuyongjiande@gmail.com>
2014-09-02 16:13:16 +0800

    Modify some pathes

--- build-xyj.sh
+++ build-xyj.sh
@@ -1,10 +1,10 @@
 make clean
-export PATH=/home/xyj/research/llvm-gcc4.2-2.9-x86_64-linux/bin:$PATH
-export PATH=/home/xyj/research/llvm-2.9-github/Release+Asserts/bin:$PATH
+export PATH=/home/xyj/research/my-llvm-gcc4.2-2.9-x86_64-linux/bin:$PATH
+export PATH=/home/xyj/research/my-llvm-2.9/Release+Asserts/bin:$PATH
 export PATH=$PATH:/usr/include/x86_64-linux-gnu
 
-./configure --with-llvm=/home/xyj/research/llvm-2.9-github \
-	--with-stp=/home/xyj/research/stp-github/build/install \
-	--with-uclibc=/home/xyj/research/klee-uclibc-github \
+./configure --with-llvm=/home/xyj/research/my-llvm-2.9 \
+	--with-stp=/home/xyj/research/my-stp/build/install \
+	--with-uclibc=/home/xyj/research/my-klee-uclibc \
 	--enable-posix-runtime \
 	&& make ENABLE_OPTIMIZED=1 -j8
